c3c85fc3897fe5672e76adebebe22f117dc14b4b
Atabegovv <atabegov98@mail.ru>
2024-04-10 23:24:30 +0300

    testing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,12 @@ import React, { FC } from 'react';
 import './App.css';
 
 const App: FC = () => {
-    return <h1 className="title">Hello webpack</h1>;
+    return (
+        <>
+            <h1 className="title">Hello webpack</h1>
+            <h3 className="subtitle"> I am done </h3>
+        </>
+    );
 };
 
 export default App;
