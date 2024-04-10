09df0f4c02a4da6de96ab17398deb566569ab32e
Atabegovv <atabegov98@mail.ru>
2024-04-10 23:48:38 +0300

    path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.[contenthash].js',
-        publicPath: './',
+        publicPath: '/',
     },
     module: {
         rules: [
